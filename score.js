6ff2b9daf6e5d08bf1153f376f1b617d190c94d0
ajanva <anvar.var@mfu.ac.th>
2024-10-15 13:35:26 +0700

    Add files via upload

---
+++ score.js
@@ -0,0 +1,68 @@
+var outputs = []
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+	// console.log(dropPosition, bounciness, size, bucketLabel)
+	// outputs.push([dropPosition, bounciness, size, bucketLabel])	
+}
+
+
+function runAnalysis() {  
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function renderHtml(bucket=0, data) {
+// 	document.getElementById("result").innerHTML = bucket
+// 	var resultsTable = document.getElementById("outputs")
+// 	outputs.forEach(output => {
+// 		resultsTable.innerHTML += "<tr><td>"+output[0]+"</td><td>"+output[1]+"</td><td>"+output[2]+"</td><td>"+output[3]+"</td></tr>"
+// 	})
+// }
+
